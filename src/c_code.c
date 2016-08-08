374a4dbbde7bc79613d490e9922bb66adf688c84
andreyshabalin <andrey.shabalin@gmail.com>
2016-08-08 02:15:02 -0400

    Added C code registration.

--- src/c_code.c
+++ src/c_code.c
@@ -7,3 +7,14 @@
 
 const int outrows = 8;
 const bool isdebug = false;
+
+
+static R_CallMethodDef callMethods[] = {
+	{"vecmatProductC", (DL_FUNC) &vecmatProductC, 2},
+	{"orthogonolizeVectorC", (DL_FUNC) &orthogonolizeVectorC, 2},
+	{NULL, NULL, 0}
+};
+
+void R_init_ACMEeqtl(DllInfo *info)	{
+	R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+}
