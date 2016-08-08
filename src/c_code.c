b39a87009439ce4b50050f5ff7267bb9e8ef8da2
andreyshabalin <andrey.shabalin@gmail.com>
2016-08-08 02:25:14 -0400

    C functions sum, sum of squares, inner product

--- src/c_code.c
+++ src/c_code.c
@@ -8,6 +8,29 @@
 const int outrows = 8;
 const bool isdebug = false;
 
+// C analog of sum(x)
+inline double sum(double *x, int n) {
+	double sum = 0;
+	for( int i=0; i<n; i++)
+		sum += x[i];
+	return(sum);
+}
+
+// C analog of sum(x^2)
+inline double sumsq(double *x, int n) {
+	double sum = 0;
+	for( int i=0; i<n; i++)
+		sum += x[i] * x[i];
+	return(sum);
+}
+
+// C analog of sum(x*y)
+inline double sumxy(double *x, double *y, int n) {
+	double sum = 0;
+	for( int i=0; i<n; i++)
+		sum += x[i] * y[i];
+	return(sum);
+}
 
 static R_CallMethodDef callMethods[] = {
 	{"vecmatProductC", (DL_FUNC) &vecmatProductC, 2},
