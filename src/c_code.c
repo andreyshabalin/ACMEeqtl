a1623a38de1bbe8c88df58a88fc2b96bff6e6dd9
andreyshabalin <andrey.shabalin@gmail.com>
2016-08-08 02:25:51 -0400

    Added C function vecmatProduct()

--- src/c_code.c
+++ src/c_code.c
@@ -8,6 +8,35 @@
 const int outrows = 8;
 const bool isdebug = false;
 
+// Product of vector 'xx' and matrix 'cc'
+// xx - n-vector
+// cc - n*ncvrt matrix
+// out - ncvrt vector
+void vecmatProduct(double *xx, double *cc, double *out, int n, int ncvrt) {
+	for( int j=0; j<ncvrt; j++) {
+		double rez = 0;		
+		for( int i=0; i<n; i++) {
+			rez += xx[i] * cc[i + j*n];
+		}
+		out[j] = rez;
+	}
+}
+
+// R wrapper for vecmatProduct, for testing
+SEXP vecmatProductC(SEXP x, SEXP cvrtqr){
+	x = PROTECT(coerceVector(x, REALSXP));
+	cvrtqr = PROTECT(coerceVector(cvrtqr, REALSXP));
+	
+	int n = length(x);
+	int ncvrt = length(cvrtqr) / length(x);
+	
+	SEXP rez = PROTECT(allocVector(REALSXP, ncvrt)); 
+	
+	vecmatProduct(REAL(x), REAL(cvrtqr), REAL(rez), n, ncvrt);
+	
+	UNPROTECT(3);
+	return(rez);
+}
 // C analog of sum(x)
 inline double sum(double *x, int n) {
 	double sum = 0;
