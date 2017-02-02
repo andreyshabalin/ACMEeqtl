334ec8db46724496aa352d5529d2df40239371c4
johnpalowitch <johnpalowitch@gmail.com>
2017-02-02 14:42:18 -0500

    Cleaned up estimate_multiSNP.

--- R/estimate_multiSNP.R
+++ R/estimate_multiSNP.R
@@ -1,23 +1,3 @@
-# Takes a SNP matrix, expression response, and covariates and 
-# estimates the corresponding multivariate ACME model via MLE
-#
-# Arguments:
-#  SNPs - a matrix of genotypes; columns are samples
-#  expr - a vector of *ACME-transformed* expression values
-#  cvrt - a matrix of covariates; columns are samples
-#  method - the optim method to use; if NULL, default will be used.
-#           Only "BFGS" currently implemented. See ?optim for details.
-#
-# Returns a list object with components:
-#  beta0hat - estimated beta0
-#  betahat - estimated SNP coefficients
-#  gamhat - estimated cvrt coefficients
-#  sigma2hat - estimated noise variance
-#  df - degrees of freedom
-#-------------------------------------------------------------------------------
-
-
-
 estimate_multiSNP <- function (SNPs, expr, cvrt, method = "BFGS") {
   
   # Prepping data
@@ -64,30 +44,6 @@ estimate_multiSNP <- function (SNPs, expr, cvrt, method = "BFGS") {
               "gamhat" = estimates[(nsnp + 2):(nsnp + ncov)],
               "sigmahat" = RSSfinal / DF,
               "DF" = DF))
-  
-}
-
-#-------------------------------------------------------------------------------
-# Sample data and usage
-
-if (FALSE) {
-  
-  n <- 1000; 
-  ncov <- 5; 
-  p <- 0.2
-  beta0 <- 1000; 
-  beta1 <- 100; 
-  beta2 <- 200
-  nsnps = 3
-  cvrt <- matrix(rnorm(ncov * n), nrow = ncov)
-  SNPs <- matrix(sample(c(0:2), n * nsnps, replace = TRUE), nrow = nsnps)
-  X <- rbind(rep(1, n), SNPs); 
-  Beta = c(beta0, beta1, beta2, 0);
-  trueGam <- rnorm(ncov);
-  expr <- crossprod(X, Beta) * exp(crossprod(cvrt, trueGam) + rnorm(n)) - 1;
-  log_expr <- log(1 + expr);
-  
-  estimate_multiSNP(SNPs, log_expr, cvrt)
   
 }
 
@@ -139,9 +95,6 @@ msACME = function (
 		n = nrow(cvrt);
 		p = ncol(cvrt);
 		
-		# Keeping gfm/sfm open for analysis loop
-		# close(gfm);
-		# close(sfm);
 	}
 	
 	### Create output matrix
