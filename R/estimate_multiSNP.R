9fdb96a39d24c175790c5c18e0e292995eb8ce46
Andrey Shabalin <ashabalin@RAMS.adp.vcu.edu>
2017-02-02 10:32:40 -0500

    More updates.

--- R/estimate_multiSNP.R
+++ R/estimate_multiSNP.R
@@ -21,41 +21,43 @@
 estimate_multiSNP <- function (SNPs, expr, cvrt, method = "BFGS") {
   
   # Prepping data
-  expr <- as.vector(expr)
-  ncov <- nrow(cvrt); nsnp <- nrow(SNPs); n <- length(expr)
-  X <- rbind(rep(1, n), SNPs)
+  expr = as.vector(expr)
+  ncov = nrow(cvrt); 
+  nsnp = nrow(SNPs); 
+  n = length(expr)
+  X = rbind(rep(1, n), SNPs)
   
   # RSS function
-  loglik <- function(BetaGam) {
-    Beta <- BetaGam[1:(nsnp + 1)]
-    Gam <- BetaGam[(nsnp + 2):(nsnp + 1 + ncov)]
-    Resid <- expr - log(crossprod(X, Beta)) - crossprod(cvrt, Gam)
-    return(sum(Resid^2))
+  loglik = function(BetaGam) {
+    Beta = BetaGam[1:(nsnp + 1)];
+    Gam = BetaGam[(nsnp + 2):(nsnp + 1 + ncov)];
+    Resid = expr - log(crossprod(X, Beta)) - crossprod(cvrt, Gam);
+    return(sum(Resid^2));
   }
   
   # Gradient function
-  Dloglik <- function(BetaGam) {
-    Beta <- BetaGam[1:(nsnp + 1)]
-    Gam <- BetaGam[(nsnp + 2):(nsnp + 1 + ncov)]
-    Resid <- expr - log(crossprod(X, Beta)) - crossprod(cvrt, Gam)
-    DBeta <- -2 * crossprod(t(X) / as.vector(crossprod(X, Beta)), Resid)
-    DGam <- -2 * cvrt %*% Resid
-    return(c(DBeta, DGam))
+  Dloglik = function(BetaGam) {
+    Beta = BetaGam[1:(nsnp + 1)];
+    Gam = BetaGam[(nsnp + 2):(nsnp + 1 + ncov)];
+    Resid = expr - log(crossprod(X, Beta)) - crossprod(cvrt, Gam);
+    DBeta = -2 * crossprod(t(X) / as.vector(crossprod(X, Beta)), Resid);
+    DGam = -2 * cvrt %*% Resid;
+    return(c(DBeta, DGam));
   }
   
   # Finding start value for beta0
-  beta0start <- exp(mean(expr))
-  parStart <- c(beta0start, rep(0, nsnp + ncov))
+  beta0start = exp(mean(expr));
+  parStart = c(beta0start, rep(0, nsnp + ncov));
   
   if (method == "BFGS") {
-    estimates <- optim(parStart, loglik, gr = Dloglik, method = "BFGS")$par
+    estimates = optim(parStart, loglik, gr = Dloglik, method = "BFGS")$par;
   } else {
-    estimates <- optim(parStart, loglik)$par
+    estimates = optim(parStart, loglik)$par;
   }
   
   # Calculating RSS and sigmahat
-  RSSfinal <- loglik(estimates)
-  DF <- n - nsnp - ncov - 1
+  RSSfinal = loglik(estimates);
+  DF = n - nsnp - ncov - 1;
   
   return(list("beta0hat" = estimates[1],
               "betahat" = estimates[2:(nsnp + 1)],
@@ -70,14 +72,20 @@ estimate_multiSNP <- function (SNPs, expr, cvrt, method = "BFGS") {
 
 if (FALSE) {
   
-  n <- 1000; ncov <- 5; p <- 0.2
-  beta0 <- 1000; beta1 <- 100; beta2 <- 200
+  n <- 1000; 
+  ncov <- 5; 
+  p <- 0.2
+  beta0 <- 1000; 
+  beta1 <- 100; 
+  beta2 <- 200
+  nsnps = 3
   cvrt <- matrix(rnorm(ncov * n), nrow = ncov)
-  SNPs <- matrix(sample(c(0:2), n * 2, replace = TRUE), nrow = 2)
-  X <- rbind(rep(1, n), SNPs); Beta = c(beta0, beta1, beta2)
-  trueGam <- rnorm(ncov)
-  expr <- crossprod(X, Beta) * exp(crossprod(cvrt, trueGam) + rnorm(n)) - 1
-  log_expr <- log(1 + expr)
+  SNPs <- matrix(sample(c(0:2), n * nsnps, replace = TRUE), nrow = nsnps)
+  X <- rbind(rep(1, n), SNPs); 
+  Beta = c(beta0, beta1, beta2, 0);
+  trueGam <- rnorm(ncov);
+  expr <- crossprod(X, Beta) * exp(crossprod(cvrt, trueGam) + rnorm(n)) - 1;
+  log_expr <- log(1 + expr);
   
   estimate_multiSNP(SNPs, log_expr, cvrt)
   
